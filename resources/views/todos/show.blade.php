2c130a3a86098e530e6db7d6103f983223cf4bf8
Yash Raikwar <yashraikwar123@gmail.com>
2023-11-03 09:12:47 +0530

    Upload Advance Todos WebApp

---
+++ resources/views/todos/show.blade.php
@@ -0,0 +1,50 @@
+
+@extends('layouts.app')
+
+@section('title')
+
+Single Todo: {{$todo->name}}
+
+@endsection
+
+@section('content')
+
+
+<h1 class="text-center my-5">
+
+    {{$todo->name}}
+</h1>
+
+<div class="row justify-content-center">
+
+    <div class="col-md-6">
+
+        <div class="card card-default">
+
+            <div class="card-header">
+                Details
+            </div>
+
+            <div class="card-body">
+
+                {{ $todo->description}}
+
+            </div>
+
+        </div>
+        <a href="/todos/{{ $todo->id}}/edit" class="btn btn-info  my-3">Edit</a>
+        <a href="/todos/{{ $todo->id}}/delete" class="btn btn-danger  my-3">Delete</a>
+
+
+    </div>
+
+</div>
+
+
+
+@endsection
+
+
+
+
+
